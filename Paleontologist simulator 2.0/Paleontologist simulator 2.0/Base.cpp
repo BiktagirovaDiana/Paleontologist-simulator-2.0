2ff4280050873f70b51d88c82e67156eb42bb265
BiktagirovaDiana <biktagirovad@yandex.ru>
2025-05-07 08:48:33 +0300

    Update Base.cpp

--- Paleontologist simulator 2.0/Paleontologist simulator 2.0/Base.cpp
+++ Paleontologist simulator 2.0/Paleontologist simulator 2.0/Base.cpp
@@ -27,9 +27,9 @@ void Base::handleEvent(sf::Event& event, sf::RenderWindow& window)
 	if (event.type == sf::Event::MouseButtonPressed)
 	{
 		if (event.mouseButton.button == sf::Mouse::Left) {
-			//позицию клика
+			//ГЇГ®Г§ГЁГ¶ГЁГѕ ГЄГ«ГЁГЄГ 
 			sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-			//координаты окна
+			//ГЄГ®Г®Г°Г¤ГЁГ­Г ГІГ» Г®ГЄГ­Г 
 			sf::Vector2f windowPos = window.mapPixelToCoords(mousePos);
 			{
 				if (IconHomeSprite.getGlobalBounds().contains(windowPos)) 
@@ -84,27 +84,7 @@ void Base::draw(sf::RenderWindow& window, sf::View GameView, sf::View UIView)
 	Player.draw(window);
 	window.draw(BusSprite);
 
-	//sf::RectangleShape expeditionRect, museumRect, shopRect;
 
-	//// Настройка прямоугольника для Expedition1
-	//expeditionRect.setSize(sf::Vector2f(TransitionExpedition1.width, TransitionExpedition1.height));
-	//expeditionRect.setPosition(TransitionExpedition1.left, TransitionExpedition1.top);
-	//expeditionRect.setFillColor(sf::Color(255, 0, 0, 100)); // Полупрозрачный красный
-
-	//// Настройка прямоугольника для Museum
-	//museumRect.setSize(sf::Vector2f(TransitionMuseum.width, TransitionMuseum.height));
-	//museumRect.setPosition(TransitionMuseum.left, TransitionMuseum.top);
-	//museumRect.setFillColor(sf::Color(0, 255, 0, 100)); // Полупрозрачный зеленый
-
-	//// Настройка прямоугольника для Shop
-	//shopRect.setSize(sf::Vector2f(TransitionShop.width, TransitionShop.height));
-	//shopRect.setPosition(TransitionShop.left, TransitionShop.top);
-	//shopRect.setFillColor(sf::Color(0, 0, 255, 100)); // Полупрозрачный синий
-
-	//// Отрисовка прямоугольников
-	//window.draw(expeditionRect);
-	//window.draw(museumRect);
-	//window.draw(shopRect);
 
 	window.setView(UIView);
 	Text.draw(window);
